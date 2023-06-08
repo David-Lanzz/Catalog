fc8c2c10eae2b24db4a1b36e3c26bc50c1c9a558
karan jain <karanjain2212@gmail.com>
2023-06-08 12:03:20 +0530

    fixed linters

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -1,6 +1,7 @@
 require_relative '../classes/game'
 require 'json'
 require 'date'
+require 'fileutils'
 
 module GameModule
   def load_game
@@ -14,42 +15,33 @@ module GameModule
     data
   end
 
-
-def save_game
+  def save_game
     data = []
     directory = './json_db'
     file_path = "#{directory}/games.json"
-  
+
     begin
       # Create the directory if it doesn't exist
-      Dir.mkdir(directory) unless Dir.exist?(directory)
-  
-      # Check if the file already exists
-      if File.exist?(file_path)
-        puts "File '#{file_path}' already exists. Overwriting the existing file..."
-      else
-        puts "File '#{file_path}' doesn't exist. Creating a new file..."
-      end
-  
+      FileUtils.mkdir_p(directory)
+
       # Process the games and prepare the data
       @games.each do |game|
         data.push({
-          'publish_date' => game.publish_date,
-          'multiplayer' => game.multiplayer,
-          'last_played_at' => game.last_played_at
-        })
+                    'publish_date' => game.publish_date,
+                    'multiplayer' => game.multiplayer,
+                    'last_played_at' => game.last_played_at
+                  })
       end
-  
-      # Write the data to the file
+
+      # Write the data to the file atomically
       File.open(file_path, 'w') do |file|
         file.puts(JSON.pretty_generate(data))
       end
-  
+
       puts 'Game data saved successfully.'
     rescue StandardError => e
       puts "Error occurred while saving the game data: #{e.message}"
       puts e.backtrace
     end
   end
-  
 end
